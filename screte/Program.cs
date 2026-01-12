95eb3d96d3af80b6de0085f0fa0576be5f54dd12
oui578475-png <oui578475@gmail.com>
2026-01-12 13:27:14 +0100

    Add project files.

---
+++ screte/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace screte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
